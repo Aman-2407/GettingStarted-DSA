478bbf16b3580e0874cbedc76cda6748378fb2ba
Aman-2407 <av986764@gmail.com>
2026-02-10 23:56:52 +0530

    Files added

---
+++ src/FibonacciSeries.java
@@ -0,0 +1,15 @@
+public class FibonacciSeries {
+    static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=8;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i <n ; i++) {
+            int next = a+b;
+            System.out.print(next + " ");
+            a=b;
+            b=next;
+
+        }
+    }
+}
