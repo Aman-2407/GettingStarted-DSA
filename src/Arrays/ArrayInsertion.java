23cb101d647d4d02d18dd9944e62a2973eea22da
Aman-2407 <av986764@gmail.com>
2026-02-22 01:41:49 +0530

    Staring with Array  22Feb CRUD Operations

--- src/Arrays/ArrayInsertion.java
+++ src/Arrays/ArrayInsertion.java
@@ -1,13 +1,15 @@
 package Arrays;
 
-public class ArrayInsertion {
-    public static void main(String[] args) {
-        int arr []= {23,45,566,28,56,67};
-        int index= 4;
-        int new_element= 450;
 
-        int newArr[] = new int[arr.length+1];
+import java.util.Arrays;
 
+class ArrayIndexInsert {
+
+    public int[] arrayinsert(int arr[], int index, int new_element) {
+
+
+        //insertion of array
+        int newArr[] = new int[arr.length + 1];
         for (int i = 0; i < index; i++) {
             newArr[i] = arr[i];
 
@@ -15,17 +17,55 @@ public class ArrayInsertion {
 
         newArr[index] = new_element;
 
-        for(int i=index;i<arr.length;i++){
-            newArr[i+1]= arr[i];
+        for (int i = index; i < arr.length; i++) {
+            newArr[i + 1] = arr[i];
         }
-        for (int no : newArr){
-            System.out.println(no+ " ");
+        return newArr;
+    }
+
+    public int[] arrayDelete(int arr[] , int index){
+        int newArr[] = new int[arr.length - 1];
+        int j=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(i==index){
+                continue;
+            }
+            newArr[j] = arr[i];
+            j++;
         }
-
-
-        for (int i = index; i <arr.length ; i++) {
-            newArr[i+1]= arr[i];
+        return  newArr;
+    }
+    public int[] arraySearch(int arrr[], int element, boolean found) {
+        for (int i = 0; i < arrr.length; i++) {
+            if (arrr[i] == element) {
+                found = true;
+                System.out.println("Element is found at index " + i);
+                break;
+            }
         }
+        if (!found) {
+            System.out.println("Element is not found in the array");
+        }
+        return  arrr;
+    }
 
+    public int[] arrayUpdate(int arr[], int index, int new_element){
+        if(index>=0 && index<arr.length){
+            arr[index] = new_element;
+        }
+        return arr;
     }
 }
+    public class ArrayInsertion {
+        public static void main(String[] args) {
+            ArrayIndexInsert a = new ArrayIndexInsert();
+            int[] result = a.arrayinsert(new int[]{34, 45, 56, 67}, 2, 100);
+            int[] result1=a.arrayDelete(new int[]{34, 45, 56, 67}, 2);
+            System.out.println("Array after Insertion: " + java.util.Arrays.toString(result));
+            System.out.println("Array after Deletion:  " + java.util.Arrays.toString(result1));
+            int[] foundIndex = a.arraySearch(new int[]{12, 340, 86, 23}, 340, false);
+            int[] updatedArr = a.arrayUpdate(new int[]{34, 45, 56, 67}, 1, 100);
+            System.out.println("Array after Update: " + java.util.Arrays.toString(updatedArr));
+        }
+    }
+
--- src/Arrays/ArraySumAndAverage.java
+++ src/Arrays/ArraySumAndAverage.java
@@ -3,14 +3,31 @@ package Arrays;
 public class ArraySumAndAverage {
     public static void main(String[] args) {
         int arr[] ={4,3,1,2};
+        int evenValuesum=0;
+        int evenValuesCount=0;
+        int evenIndexSum=0;
+        int evenIndexCount=0;
         int sum=0;
         for (int i=0;i<=arr.length-1;i++){
-           sum= sum+arr[i]   ;
+            sum+=arr[i];
+            if(arr[i]%2==0){
+                evenValuesum= evenValuesum+arr[i]   ;
+                evenValuesCount++;
+            }
+            if (i%2==0) {
+                    evenIndexSum = evenIndexSum+arr[i] ;
+                    evenIndexCount++;
+            }
+
+
         }
-        System.out.println("Sum of given array is" +sum);
+        //Calculating Averages
+        double avgVal = (arr.length > 0) ? (double) sum / arr.length : 0;
+        double avgEvenVal = (evenValuesCount > 0) ? (double) evenValuesum / evenValuesCount : 0;
+        double avgEvenIdx = (evenIndexCount > 0) ? (double) evenIndexSum / evenIndexCount : 0;
+        System.out.println("Sum of Values: " + sum + " | Avg: " + avgVal);
+        System.out.println("Sum of Even Values: " + evenValuesum + " | Avg: " + avgEvenVal);
+        System.out.println("Sum of Even Indices: " + evenIndexSum + " | Avg: " + avgEvenIdx);
 
-        double avg = (double)sum/arr.length;
-
-        System.out.println("Average is" + avg);
     }
 }
