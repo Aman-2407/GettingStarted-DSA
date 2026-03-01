597d9d66eef90125e39710ef9887b69ddb1eb4a0
Aman-2407 <av986764@gmail.com>
2026-03-02 01:41:04 +0530

    Binary Search with Celing, First Occurance, Last Occurance

--- src/Arrays/ArrayBinarySearch.java
+++ src/Arrays/ArrayBinarySearch.java
@@ -3,32 +3,98 @@ package Arrays;
 
 import java.util.Arrays;
 
-public class ArrayBinarySearch {
-    void main() {
-        int arr[] = {23, 34, 53, 12, 78, 45, 23};
-        int first_element = 0;
-        int last_element = arr.length - 1;
-        int value=23;
-        boolean found = false;
+class BinarySearchUtiltiy{
+    public int binarySearch(int[] arr, int value){
         Arrays.sort(arr);
-        IO.println("Sorted Array is "+ Arrays.toString(arr));
-        while (first_element <= last_element) {
-            int middle_element = first_element + (last_element- first_element) / 2; // Dividing the array
-            if (arr[middle_element] == value) {
-                IO.println("Array found at index " + middle_element);
-                found = true;
-                break;
+        int low = 0;
+        int high = arr.length-1;
+        int mid = 0;
+        while(low<=high){
+            mid = low +(high-low)/2;
+            if(arr[mid] == value){
+                return mid;
             }
-            if(value > arr[middle_element]) {
-                first_element = middle_element + 1;
+            if(arr[mid] > value){
+                high = mid-1;
             }
-            else{
-                last_element = middle_element-1;
+            else if(arr[mid] < value){
+                low = mid+1;
             }
         }
-if(!found){
-    IO.println("Value is not present inside given Array ");
+        return mid;
+    }
+    public int binarySearchFirstandLastOcc(int[] arr, int key, boolean isFirst){
+        Arrays.sort(arr);
+        int low = 0;
+        int high = arr.length-1;
+        int mid = 0;
+        int result = -1;
+        while(low<=high) {
+            mid = low + (high - low) / 2;
+            if (arr[mid] == key) {
+                result = mid;
+                if (isFirst) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (key > arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public int findCeiling(int[] arr, int key){
+        int low = 0;
+        int high = arr.length-1;
+        int mid = 0;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(key > arr[mid]){
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return (low<arr.length)?low:-1;
+    }
+   
+}
+
+public class ArrayBinarySearch {
+    void main() {
+        int[] arr= {45,34,23,56,45,67,23};
+        Arrays.sort(arr);
+        int key=45;
+        BinarySearchUtiltiy obj = new BinarySearchUtiltiy();
+
+        IO.println("Sorted Array : " + Arrays.toString(arr));
+        IO.println("Searching for key : " + key);
+        int value= obj.binarySearch(arr, key);
+        IO.println("Element found at index : "+value);
+        int first = obj.binarySearchFirstandLastOcc(arr,key,true);
+        IO.println("First Occurrence of the Element "+key + " is : "+first);
+        int last = obj.binarySearchFirstandLastOcc(arr,key,false);
+        IO.println("Last Occurrence of the Element "+key+" is : "+last);
+        if(first ==-1){
+            IO.println("Count is 0");
+        }
+        else {
+            int total= (last-first)+1;
+            IO.println("Count of the key "+key+ " is "+total);
+        }
+        int celi= obj.findCeiling(arr,key);
+        IO.println("Ceiling Element found at index : "+celi);
+     
+        
+
 }
 
     }
-}
